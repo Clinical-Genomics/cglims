79aea492a3a1ef5caebe1af2075d520affe319e8
Robin Andeer <robin.andeer@gmail.com>
2016-09-23 10:49:23 +0200

    add case id to output

--- cglims/cli/commands.py
+++ cglims/cli/commands.py
@@ -45,6 +45,9 @@ def get(context, pretty, identifier, fields):
         values['date_received'] = datetime(*date_parts)
         values['project_name'] = sample.project.name
         values['sex'] = SEX_MAP.get(values['Gender'])
+        if 'customer' in values and 'familyID' in values:
+            values['case_id'] = "{}-{}".format(values['customer',
+                                               values['familyID']])
 
         if fields:
             output = ' '.join(values[field] for field in fields
