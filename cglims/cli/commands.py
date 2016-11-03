1521ec1ede7ecd24df75226ff367f59b864a46cc
Robin Andeer <robin.andeer@gmail.com>
2016-11-03 10:07:03 +0100

    mark if sample is cancelled

--- cglims/cli/commands.py
+++ cglims/cli/commands.py
@@ -68,6 +68,8 @@ def get(context, condense, identifier, fields):
                 dump = fix_dump(raw_dump)
                 click.echo(click.style('>>> Sample: ', fg='red'), nl=False)
                 click.echo(click.style(sample.id, bold=True, fg='red'))
+                if sample.udf.get('cancelled') != 'yes':
+                    click.echo(click.style('CANCELLED', bold=True, fg='yellow'))
             click.echo(dump)
 
 
