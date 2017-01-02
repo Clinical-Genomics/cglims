5b4be6ee15200c5286f1fc370e9ba64e757b053a
Robin Andeer <robin.andeer@gmail.com>
2017-01-02 10:29:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cglims',
-    version='1.0.0',
+    version='1.1.0',
     description='Microservice to interface with LIMS',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
