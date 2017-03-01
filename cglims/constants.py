760adb54052ea6ac48214e7b088aac3e7bb58205
Robin Andeer <robin.andeer@gmail.com>
2017-03-01 08:59:35 +0100

    add option to sexmap

--- cglims/constants.py
+++ cglims/constants.py
@@ -5,4 +5,4 @@ NO_BASES = 3100000000
 BASES_PER_READ = 150
 READS_PER_1X = NO_BASES / BASES_PER_READ
 
-SEX_MAP = {'F': 'female', 'M': 'male', 'Unknown': 'unknown'}
+SEX_MAP = {'F': 'female', 'M': 'male', 'Unknown': 'unknown', 'unknown': 'unknown'}
