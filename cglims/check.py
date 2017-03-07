19f4437b166aa60d5157b50b764784ff02c9a20e
Robin Andeer <robin.andeer@gmail.com>
2017-03-07 14:21:13 +0100

    udpate check command

--- cglims/check.py
+++ cglims/check.py
@@ -28,23 +28,53 @@ def samples(context, project, limit):
 @click.option('-u', '--update', is_flag=True, help='update sample UDFs')
 @click.option('-f', '--force', is_flag=True, help='update existing values')
 @click.option('-v', '--version', type=int, help='application tag version')
+@click.option('-s', '--source',
+              type=click.Choice(['sample', 'project', 'process']),
+              default='sample')
 @click.argument('lims_id')
 @click.pass_context
-def check(context, update, version, force, lims_id):
-    """Check samples in a project."""
+def check(context, update, version, force, source, lims_id):
+    """Check LIMS sample or all samples in a process."""
     lims = api.connect(context.obj)
-    # get sample in the project
-    lims_sample = lims.sample(lims_id)
-    check_samplename(lims_sample)
-    check_duplicatename(lims, lims_sample)
-    check_capturekit(lims_sample)
-    check_familymembers(lims, lims_sample)
+    if source == 'sample':
+        lims_samples = [{'sample': lims.sample(lims_id)}]
+    elif source == 'process':
+        lims_samples = process_samples(lims_id)
+    elif source == 'project':
+        lims_samples = ({'sample': sample} for sample in
+                        lims.get_samples(projectlimsid=lims_id))
 
-    if update:
-        set_missingreads(lims_sample, force=force)
-        set_trioapptag(lims, lims_sample)
-        if version:
-            set_apptagversion(lims_sample, version, force=force)
+    for sample in lims_samples:
+        lims_sample = sample['sample']
+        results = []
+        log.info("checking sample: %s (%s)", lims_sample.id, lims_sample.name)
+        log.debug('checking sample name...')
+        results.append(check_samplename(lims_sample))
+        log.debug('checking duplicate external sample name...')
+        results.append(check_duplicatename(lims, lims_sample))
+        log.debug('checking capture kit (extenal sequencing)...')
+        results.append(check_capturekit(lims_sample))
+        log.debug('checking family members...')
+        results.append(check_familymembers(lims, lims_sample))
+
+        if update:
+            log.debug('updating missing reads...')
+            set_missingreads(lims_sample, force=force)
+            log.debug('checking if update to trio tag is possible...')
+            set_trioapptag(lims, lims_sample)
+            if version:
+                log.debug('updating application tag version...')
+                set_apptagversion(lims_sample, version, force=force)
+
+            if 'artifact' in sample:
+                if False in results:
+                    log.warn("sample check FAILED: %s", lims_sample.id)
+                    sample['artifact'].qc_flag = 'FAILED'
+                else:
+                    log.info("sample check PASSED: %s", lims_sample.id)
+                    sample['artifact'].qc_flag = 'PASSED'
+                sample['artifact'].put()
+
 
 
 def set_missingreads(lims_sample, force=False):
@@ -68,7 +98,7 @@ def set_apptagversion(lims_sample, version, force=False):
         log.warn("application tag version already set: %s", current_version)
     else:
         lims_sample.udf['Application Tag Version'] = version
-        log.info("updating application tag version")
+        log.info("updating application tag version: %s", version)
         lims_sample.put()
 
 
@@ -99,20 +129,26 @@ def check_samplename(lims_sample):
     no_dash = lims_sample.name.replace('-', '')
     if not no_dash.isalnum():
         log.error("sample name not OK: %s", lims_sample.name)
+        return False
+    else:
+        return True
 
 
 def check_duplicatename(lims, lims_sample):
     """Check if the same customer has sent in a sample with the same id."""
+    result = True
     samples = lims.get_samples(name=lims_sample.name,
                                udf={'customer': lims_sample.udf['customer']})
     for other_sample in samples:
         if other_sample.id != lims_sample.id:
             # same sample id twice!
             if other_sample.udf.get('cancelled') == 'yes':
-                log.debug("duplicate sample but the other is cancelled")
+                log.info("duplicate sample but the other is cancelled")
             else:
                 log.error("sample name duplicate: %s | %s", lims_sample.id,
                           other_sample.id)
+                result = False
+    return result
 
 
 def check_capturekit(lims_sample):
@@ -123,10 +159,13 @@ def check_capturekit(lims_sample):
         capture_kit = lims_sample.udf['Capture Library version']
         if capture_kit == 'NA':
             log.error("capture kit not filled in for external sample")
+            return False
+    return True
 
 
 def check_familymembers(lims, lims_sample):
     """Check if sample has family memeber but no relations to them."""
+    result = True
     related_samples = lims.case(lims_sample.udf['customer'],
                                 lims_sample.udf['familyID'])
     if len(related_samples) > 1:
@@ -148,6 +187,7 @@ def check_familymembers(lims, lims_sample):
                             log.error("original sample, %s", lims_sample.id)
                             log.error("family: %s", lims_sample.udf['familyID'])
                             log.error("other sample (missing): %s", related_id)
+                            result = False
                         else:
                             log.debug("found related sample: %s = %s", udf_key,
                                       samples[related_id].id)
@@ -159,3 +199,12 @@ def check_familymembers(lims, lims_sample):
                 log.debug("samples part of cancer combo")
             else:
                 log.error("samples in 'family' not related, tumor/normal?")
+                result = False
+    return result
+
+
+def process_samples(lims_process):
+    """Retrieve LIMS input samples from a process."""
+    for artifact in lims_process.all_inputs():
+        for lims_sample in artifact.samples:
+            yield {'sample': lims_sample, 'artifact': artifact}
