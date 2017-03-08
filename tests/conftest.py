954689d4236a07497152f8533eb4ca2ca5d816e2
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-03-08 13:16:21 +0100

    Add tests for apptag

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,35 @@ from cglims.apptag import ApplicationTag
 
 
 @pytest.fixture
-def apptag():
+def apptag_wgs():
     raw_tag = 'WGSPCFC030'
     return ApplicationTag(raw_tag)
+
+
+@pytest.fixture
+def apptag_wes():
+    raw_tag = 'EXOSXTR100'
+    return ApplicationTag(raw_tag)
+
+
+@pytest.fixture
+def apptag_microbial():
+    raw_tag = 'MWGNXTR003'
+    return ApplicationTag(raw_tag)
+
+
+@pytest.fixture
+def apptag_external():
+    raw_tag = 'EXXCUSR000'
+    return ApplicationTag(raw_tag)
+
+
+@pytest.fixture
+def apptags():
+    return {
+        'wgs': apptag_wgs(),
+        'wes': apptag_wes(),
+        'microbial': apptag_microbial(),
+        'external': apptag_external()
+    }
+
