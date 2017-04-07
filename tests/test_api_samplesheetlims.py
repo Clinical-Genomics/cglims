e8226336da6fced89ac8ef52961a92c270ab5051
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-04-07 22:54:49 +0200

    Add test_api_samplesheetlims.py

---
+++ tests/test_api_samplesheetlims.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from cglims.api import SamplesheetHandler
+
+def test_samplesheet():
+    samplesheethandler = SamplesheetHandler()
+
+    assert samplesheethandler._get_placement_lane('4:1') == 4
+    assert samplesheethandler._get_index('G07 - D707-D507 (CTGAAGCT-CAGGACGT)') == 'CTGAAGCT-CAGGACGT'
