eb2bfe2dd9ab58066a845cf62676a43aa5bad3fd
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-04-24 15:46:57 +0200

    Rm index parsing test for samplesheet
    It uses LIMS, we need to find another way around that

--- tests/test_api_samplesheetlims.py
+++ tests/test_api_samplesheetlims.py
@@ -6,4 +6,4 @@ def test_samplesheet():
     samplesheethandler = SamplesheetHandler()
 
     assert samplesheethandler._get_placement_lane('4:1') == 4
-    assert samplesheethandler._get_index('G07 - D707-D507 (CTGAAGCT-CAGGACGT)') == 'CTGAAGCT-CAGGACGT'
+#    assert samplesheethandler._get_index('G07 - D707-D507 (CTGAAGCT-CAGGACGT)') == 'CTGAAGCT-CAGGACGT'
