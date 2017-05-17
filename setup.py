6f36fb79a8e7fb0455209d319b6a260bce1c5187
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-05-17 14:50:47 +0200

    Bumpversion 1.2.0 -> 1.3.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cglims',
-    version='1.2.0',
+    version='1.3.0',
     description='Microservice to interface with LIMS',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
