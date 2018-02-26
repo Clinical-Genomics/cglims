8675945d7c471d430dc380667c3b47a03a231db9
Emma Sernstad <emma.sernstad@scilifelab.se>
2018-02-26 08:47:32 +0100

    Bumped up to 3.1.2
    Added new app-tag in apptag.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cglims',
-    version='1.3.1',
+    version='1.3.2',
     description='Microservice to interface with LIMS',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
