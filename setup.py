969e10b374adb89250404b5dc6eca242acff7309
emmasernstad <emma.sernstad@scilifelab.se>
2018-08-15 13:11:11 +0200

    Bump version: 1.3.2 → 1.3.3

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cglims',
-    version='1.3.2',
+    version='1.3.3',
     description='Microservice to interface with LIMS',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
